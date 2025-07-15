50ad513befad136d54616e914185efc437cbd262
viraldhakan <viraldhakan143@gmail.com>
2025-07-15 12:49:35 +0530

    integration test case

--- src/test/java/Katalon/testbase/TestBase.java
+++ src/test/java/Katalon/testbase/TestBase.java
@@ -9,24 +9,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 
-
 import java.io.ByteArrayInputStream;
 
 public class TestBase {
 
 
     @BeforeMethod
-    protected void init(){
+    protected void init() {
         DriverManagerTL.init();
     }
 
     @AfterMethod
-    protected  void down(){
+    protected void down() {
         DriverManagerTL.down();
     }
 
-    protected void takeScreenshort(WebDriver driver){
-        Allure.addAttachment("Screenshort",new ByteArrayInputStream
+    protected void takeScreenshort(WebDriver driver) {
+        Allure.addAttachment("Screenshort", new ByteArrayInputStream
                 (((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
     }
 
